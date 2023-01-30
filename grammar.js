1edeccb6385de63bbd5f496b2696d7ccc4c35d15
Lewis Russell <lewis6991@gmail.com>
2023-01-30 11:06:39 +0000

    allow identifiers in string concatentations
    Resolves errors in strings of the form:
    
       ("failed in line %" PRIdLINENR)

--- grammar.js
+++ grammar.js
@@ -1017,7 +1017,7 @@ module.exports = grammar({
 
     concatenated_string: $ => seq(
       $.string_literal,
-      repeat1($.string_literal)
+      repeat1(choice($.string_literal, $.identifier))
     ),
 
     string_literal: $ => seq(
